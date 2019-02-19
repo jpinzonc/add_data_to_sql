8afa8fc36ae445764d49b5047242681313e6cf21
jpinzonc <jpinzon@hotmail.com>
2019-02-18 18:43:31 -0600

    first commit

---
+++ forms.py
@@ -0,0 +1,23 @@
+# forms.py
+
+from wtforms import Form, StringField, SelectField, validators
+
+class MusicSearchForm(Form):
+    choices = [('Artist', 'Artist'),
+               ('Album', 'Album'),
+               ('Price', 'Price')]
+    select = SelectField('Search for music:', choices=choices)
+    search = StringField('')
+
+
+class AlbumForm(Form):
+    competitors = [('LOWES', 'LOWES'),
+                   ('HOME DEPOT', 'HOME DEPOT'),
+                   ('ELLIOTS', 'ELLIOTS'),
+                   ('PLATT', 'PLATT')
+                   ]
+    artist = StringField('Artist')
+    title = StringField('Title')
+    release_date = StringField('Release Date')
+    Price = StringField('Price')
+    Competitors_name = SelectField('Competitors', choices=competitors)
